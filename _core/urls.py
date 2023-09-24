f743de7c16bec9ee3e070898c6cfe0dc04e3d604
Roki Miftah Kamaludin <rokimiftah@gmail.com>
2023-09-24 14:24:42 +0700

    add crmbasic urls to core urls

--- _core/urls.py
+++ _core/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", include("APP.urls")),
+    path("", include("crmbasic.urls")),
     # path("__reload__/", include("django_browser_reload.urls")),
 ]
